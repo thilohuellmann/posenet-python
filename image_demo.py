be222a0824c93de158d4c15c969bb2ceb7ab2062
Thilo Huellmann <thilo@colabel.com>
2019-10-02 19:34:31 +0200

    Update image_demo.py

--- image_demo.py
+++ image_demo.py
@@ -47,25 +47,36 @@ def main():
                 output_stride=output_stride,
                 max_pose_detections=10,
                 min_pose_score=0.25)
+            
+            print('pose scores:')
+            print(pose_scores)
+            
+            print('keypoint scores:')
+            print(keypoint_scores)
+            
+            print('keypoint coords:')
+            print(keypoint_coords)
+            break
+            
+#             keypoint_coords *= output_scale
 
-            keypoint_coords *= output_scale
+#             if args.output_dir:
+#                 draw_image = posenet.draw_skel_and_kp(
+#                     draw_image, pose_scores, keypoint_scores, keypoint_coords,
+#                     min_pose_score=0.25, min_part_score=0.25)
+                
+#                 # try on white background...
+#                 #cv2.imwrite(os.path.join(args.output_dir, os.path.relpath(f, args.image_dir)), draw_image)
 
-            if args.output_dir:
-                draw_image = posenet.draw_skel_and_kp(
-                    draw_image, pose_scores, keypoint_scores, keypoint_coords,
-                    min_pose_score=0.25, min_part_score=0.25)
-
-                cv2.imwrite(os.path.join(args.output_dir, os.path.relpath(f, args.image_dir)), draw_image)
-
-            if not args.notxt:
-                print()
-                print("Results for image: %s" % f)
-                for pi in range(len(pose_scores)):
-                    if pose_scores[pi] == 0.:
-                        break
-                    print('Pose #%d, score = %f' % (pi, pose_scores[pi]))
-                    for ki, (s, c) in enumerate(zip(keypoint_scores[pi, :], keypoint_coords[pi, :, :])):
-                        print('Keypoint %s, score = %f, coord = %s' % (posenet.PART_NAMES[ki], s, c))
+#             if not args.notxt:
+#                 print()
+#                 print("Results for image: %s" % f)
+#                 for pi in range(len(pose_scores)):
+#                     if pose_scores[pi] == 0.:
+#                         break
+#                     print('Pose #%d, score = %f' % (pi, pose_scores[pi]))
+#                     for ki, (s, c) in enumerate(zip(keypoint_scores[pi, :], keypoint_coords[pi, :, :])):
+#                         print('Keypoint %s, score = %f, coord = %s' % (posenet.PART_NAMES[ki], s, c))
 
         print('Average FPS:', len(filenames) / (time.time() - start))
 
